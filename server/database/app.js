93c174dff84662e1e36a2a0e54ef4d709b2a9e1f
Gerardo <barranteslorenzo6@gmail.com>
2025-08-28 21:01:30 -0400

    BackEnd

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,35 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        const dealerships = await Dealerships.find({});
+        res.json(dealerships);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    const state = req.params.state;
+    try {
+        const dealerships = await Dealerships.find({ state: state });
+        res.json(dealerships);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    const id = req.params.id;
+    try {
+        const dealerships = await Dealerships.find({ id: id });
+        res.json(dealerships);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+
 });
 
 //Express route to insert review
