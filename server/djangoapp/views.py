3fa3fb80e0a89c085d9b1536ceb3e6971bb719b8
BC-GELO <barranteslorenzo6@gmail.com>
2025-08-29 13:53:51 -0600

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,7 +15,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
-from .restapis import get_request, analyze_review_sentiments # , post_review
+from .restapis import get_request, analyze_review_sentiments  # , post_review
 
 
 # Get an instance of a logger
@@ -131,12 +131,12 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous == False):
-        data = json.loads(request.body)
+    if (request.user.is_anonymous is False):
+        # data = json.loads(request.body)
         try:
             # response = post_review(data)
             return JsonResponse({"status": 200})
-        except:
+        finally:
             return JsonResponse({"status": 401,
                                  "message": "Error in posting review"})
     else:
