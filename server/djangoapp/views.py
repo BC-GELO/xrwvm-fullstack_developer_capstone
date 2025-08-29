0e69a75354384b80da7cf41ee74a3b5ca1d12635
BC-GELO <barranteslorenzo6@gmail.com>
2025-08-29 12:29:44 -0600

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
 from django.shortcuts import render
-#from django.http import HttpResponse, HttpResponseRedirect
+  #from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
-#from django.shortcuts import get_object_or_404, redirect
+  #from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth import logout
-#from django.contrib import messages
-#from datetime import datetime
+  #from django.contrib import messages
+  #from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
